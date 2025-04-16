e6ae5db8738f5ed9f86d54c4c4c5758ea870ecde
Benny254 <asshimbenny01@gmail.com>
2025-04-16 11:20:30 +0300

    update

---
+++ wardrobe/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Best Wardrobe</h1>
+    <nav>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  margin-bottom: 20px;
+}
+</style>
